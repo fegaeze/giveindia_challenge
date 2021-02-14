8e15079f46aeb3b6fe0cef239e6ecb99c7dcc000
Chioma Nkem-Eze <jchioma@quidax.com>
2021-02-14 10:52:33 +0100

    reverted back to stop videofor when playlist is over

--- src/js/state.js
+++ src/js/state.js
@@ -32,7 +32,7 @@ export const removeFromPlayList = (id) => {
 
 const handleStateChanges = (prevState, currentState) => {
   if (player && currentState.length === 0) {
-    player.loadPlaylist({ playlist: []})
+    player.stopVideo();
   } else if (player && currentState.length === 1) {
     loadPlaylist(player);
   } else if (prevState.length > currentState.length) {
